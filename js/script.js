544aed344fe7b3498c801625909d9425c2c813c6
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-03-09 13:49:07 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const endDate = "25 March 2024 07:00 PM"
+
+document.getElementById("endDate").innerText = endDate;
+
+const inputs = document.querySelectorAll('input');
+
+const clock = () =>{
+const end = new Date(endDate)
+const now = new Date()
+
+const diff = (end - now) / 1000 ;
+if(diff < 0 ) return ;
+
+console.log(diff)
+// Days
+inputs[0].value = Math.floor(diff/3600/24); //Hours
+inputs[1].value = Math.floor((diff / 3600)%24 );//Minute
+inputs[2].value = Math.floor(diff/60)%60; //Seconds
+inputs[3].value = Math.floor(diff%60); //remaining seconds
+
+}
+
+clock()
+
+setInterval(
+    () =>{
+        clock()
+    },1000
+)
